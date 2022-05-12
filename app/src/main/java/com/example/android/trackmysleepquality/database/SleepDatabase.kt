ea661cf61dc61159542b485434fd7c508eccb373
Thiago Galindo <ecthiagoufpb@gmail.com>
2022-05-12 12:02:40 +0000

    [FIX] Add database notation to fix database_impl issue.

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -17,9 +17,11 @@
 package com.example.android.trackmysleepquality.database
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Database(entities= [SleepNight::class], version = 1, exportSchema = false)
 abstract class SleepDatabase : RoomDatabase() {
     abstract val sleepDatabaseDao: SleepDatabaseDao
 
